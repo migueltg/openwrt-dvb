b88886fa3f96ccf80090a8d25b69d35301c2280c
Miguel Tejedor <miguel@mitegu.com>
2019-11-17 11:54:37 +0100

    Update media_build and media_tree repositories

--- v4l-dvb/origin/linuxtv.mk
+++ v4l-dvb/origin/linuxtv.mk
@@ -2,11 +2,11 @@ PKG_NAME := v4l-dvb-linuxtv
 
 PKG_SOURCE_PROTO := git
 PKG_SOURCE_URL := git://linuxtv.org/media_build.git
-PKG_SOURCE_VERSION := 0c8bb27f3aaa682b9548b656f77505c3d1f11e71
-PKG_SOURCE_DATE := 2018-10-19
+PKG_SOURCE_VERSION := efba365ba11b958a6bf6fb4b397942f9461cefca
+PKG_SOURCE_DATE := 2019-11-06
 
 V4L_SOURCE_URL := git://linuxtv.org/media_tree.git
-V4L_SOURCE_VERSION := 3b796aa60af087f5fec75aee9b17f2130f2b9adc
+V4L_SOURCE_VERSION := 6f51fdfd8229d5358c2d6e272cf73478866e8ddc
 V4L_REPO_NAME := linux-media-linuxtv
 V4L_SOURCE_FILE := $(V4L_REPO_NAME)-$(PKG_SOURCE_DATE)-$(shell printf '%.8s' $(V4L_SOURCE_VERSION)).tar.bz2
 
