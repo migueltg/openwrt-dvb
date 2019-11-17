5f60cec7f5627f25fdda7e0c186d9b283df1ab68
Miguel Tejedor <miguel@mitegu.com>
2019-11-17 12:35:24 +0100

    Update tdsdtv repositories

--- v4l-dvb/origin/tbsdtv.mk
+++ v4l-dvb/origin/tbsdtv.mk
@@ -2,11 +2,11 @@ PKG_NAME := v4l-dvb-tbsdtv
 
 PKG_SOURCE_PROTO := git
 PKG_SOURCE_URL := https://github.com/tbsdtv/media_build.git
-PKG_SOURCE_VERSION := 4735369fca22122cbc4dffd16c36026747563b0b
-PKG_SOURCE_DATE := 2018-10-16
+PKG_SOURCE_VERSION := 91dc8972d37efdc12499530ce991cbe3cf3cd64a
+PKG_SOURCE_DATE := 2019-09-13
 
 V4L_SOURCE_URL := https://github.com/tbsdtv/linux_media.git
-V4L_SOURCE_VERSION := 23087976705d594febef88e9160c3eb76abb2d48
+V4L_SOURCE_VERSION := 59b57115beb2195d7a0ed11323ca0ee9bf5d5679
 V4L_REPO_NAME := linux-media-tbsdtv
 V4L_SOURCE_FILE := $(V4L_REPO_NAME)-$(PKG_SOURCE_DATE)-$(shell printf '%.8s' $(V4L_SOURCE_VERSION)).tar.bz2
 
